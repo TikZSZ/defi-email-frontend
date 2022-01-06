a635218dc1bc3a3da800f1688bb9cee5f758bc19
TikZSZ <repackergame@gmail.com>
2022-01-06 08:00:45 +0530

    attempt to fix constructor undefined

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
+  build:{
+    target:'es2015'
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
